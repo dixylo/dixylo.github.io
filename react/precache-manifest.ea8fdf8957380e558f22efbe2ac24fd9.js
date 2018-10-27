4392bdb6d492fc66fc1a4affc6f54e1e0a58e81b
dixylo <bixylim@gmail.com>
2018-10-27 16:03:20 +1300

    Site updated: 2018-10-27 16:03:20

---
+++ react/precache-manifest.ea8fdf8957380e558f22efbe2ac24fd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "82ffc2b904714b7d896f",
+    "url": "/static/js/main.82ffc2b9.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "82ffc2b904714b7d896f",
+    "url": "/static/css/main.dd39b4f5.chunk.css"
+  },
+  {
+    "revision": "f7c3585e92fa62f0e6d21091723e058e",
+    "url": "/index.html"
+  }
+];
